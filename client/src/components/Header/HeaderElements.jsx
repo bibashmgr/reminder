26ae8a980c20137c56c9ff0611784ef6a3e222b4
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-01-23 20:07:23 +0545

    Adding Header

---
+++ client/src/components/Header/HeaderElements.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.header`
+    height: 10vh;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #2b2b2b;
+    color: #ffffff;
+`
+export const HeaderTitle = styled.div`
+    font-size: 1.5rem;
+    font-weight: 700;
+`
---
+++ client/src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// elements
+import { HeaderContainer, HeaderTitle } from './HeaderElements';
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <HeaderTitle>Reminder</HeaderTitle>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
