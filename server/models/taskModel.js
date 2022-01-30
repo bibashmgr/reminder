a7c18d86e25d50a8412ea3521cc2d6e143ffcb4e
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-01-30 18:50:44 +0545

    Cleaning up

--- server/models/taskModel.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const taskSchema = mongoose.Schema({
-  name: String,
-});
-
-const taskModel = mongoose.model('taskModel', taskSchema);
-
-export default taskModel;
