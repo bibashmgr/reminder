e147de0324fcb7e3cab275a3aaeaa84be103807b
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-01-30 18:50:30 +0545

    Making some changes

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import dotenv from 'dotenv';
 
 // routes:
@@ -8,6 +10,10 @@ import taskRoutes from './routes/tasks.js';
 const app = express();
 
 // use:
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
 app.use('/tasks', taskRoutes);
 
 // constants:
