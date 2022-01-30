b39de035096d43d641c24a5f007f2aac4cfdc825
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-01-30 18:51:25 +0545

    Updating some changes

--- server/controllers/tasks.js
+++ server/controllers/tasks.js
@@ -1,25 +1,21 @@
-import taskModel from '../models/taskModel.js';
+import TaskMessage from '../models/taskMessage.js';
 
 export const getTasks = async (req, res) => {
   try {
-    const tasksModel = await taskModel.find();
-    // console.log(tasksModel);
-    res.status(200).json(tasksModel);
+    const taskmessages = await TaskMessage.find();
+    res.status(200).json(taskmessages);
   } catch (error) {
-    // console.log(error.message);
     res.status(401).json({ message: error.message });
   }
 };
 
 export const createTask = async (req, res) => {
   const task = res.body;
-  const newTask = new taskModel(task);
+  const newTask = new TaskMessage(task);
   try {
-    // console.log(newTask);
     await newTask.save();
     res.status(201).json(newTask);
   } catch (error) {
-    // console.log(error.message);
     res.status(409).json({ message: error.message });
   }
 };
