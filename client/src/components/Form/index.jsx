65252491ede3f1f8704f7d1c8980d4d2d20ede2d
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-01-30 18:51:49 +0545

    Importing api

--- client/src/components/Form/index.jsx
+++ client/src/components/Form/index.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 
 // elements
 import { FormContainer, FormBox, InputField, AddButton } from './FormElements';
 
+// api
+// import { createTask } from '../../api/index.js';
+
 const Form = () => {
+
+  const [taskData, setTaskData] = useState({
+    taskname: '',
+  });
+
+  const handleChange = (e) => {
+    setTaskData({ taskname: e.target.value });
+  }
+
+  const handleSubmit = (e) => {
+    const url = 'http://localhost:4000/tasks';
+    e.preventDefault();
+    
+    axios.post(url, taskData)
+      .then(res => console.log(res))
+      .catch(error => console.log(error.message));
+  }
+
   return (
     <FormContainer>
-      <FormBox>
-        <InputField type='text' placeholder='Enter task' />
+      <FormBox onSubmit={handleSubmit}>
+        <InputField type='text' name='taskname' placeholder='Enter task' value={taskData.taskname} onChange={handleChange} />
         <AddButton type='submit' value='Add task' />
       </FormBox>
     </FormContainer>
--- client/src/components/List/index.jsx
+++ client/src/components/List/index.jsx
@@ -1,23 +1,39 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 // elements
 import { ListContainer, ListBox, Item, ItemContent, ContentCheckBox, ContentText , ItemIcons, EditIcon, DeleteIcon } from './ListElements';
 
 const List = () => {
+  
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const url = 'http://localhost:4000/tasks';
+    axios.get(url)
+      .then(res => res.data)
+      .then(data => setTasks(data))
+      .catch(error => console.log(error.message));
+    }, []);
+
   return (
       <ListContainer>
           <ListBox>
-            {/* <TimeBox>9:00 AM</TimeBox> */}
-            <Item>
-              <ItemContent>
-                <ContentCheckBox type='checkbox' />
-                <ContentText type='text' value='Do AINN presentation' disabled />
-              </ItemContent>
-              <ItemIcons>
-                <EditIcon />
-                <DeleteIcon />
-              </ItemIcons>
-            </Item>
+            {tasks.map((task, index) => {
+              return(
+                <Item key={index}>
+                  <ItemContent>
+                    <ContentCheckBox type='checkbox' />
+                    <ContentText type='text' value={task.taskname} disabled />
+                  </ItemContent>
+                  <ItemIcons>
+                    <EditIcon />
+                    <DeleteIcon />
+                  </ItemIcons>
+                </Item>
+              )
+            })}
           </ListBox>
       </ListContainer>
   );
